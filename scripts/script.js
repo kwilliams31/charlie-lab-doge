ca74728fe2642d9ec397e81ecf81bdf80bafe1af
Kristina <111464160+kwilliams31@users.noreply.github.com>
2022-09-12 16:41:29 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -8,8 +8,6 @@ $(document).ready(function() {
     console.log("user clicked doge one"); 
     $("#outputMsg").text("you lose not wow");
     $("#outputDiv").toggle();
-
-  
     
       // dogeOne click closing
        });
@@ -20,9 +18,13 @@ $(document).ready(function() {
     console.log("user clicked doge two");  
     $("#outputMsg").text("you has WOW!!");
     $("#outputDiv").toggle();
+    var div = $("#dogeTwo");
+    div.animate({height: "400px", opacity: "0.4"}, "slow");
+    div.animate({width: "500px", opacity: "0.8"}, "slow");
+    div.animate({height: "450px", opacity: "0.4"}, "slow");
+    div.animate({width: "750px", opacity: "1"}, "slow");
     
-
-             
+    
 
        // dogeTwo click closing
       });  
